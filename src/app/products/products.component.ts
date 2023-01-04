72e786f602d9d722cc2570ab0751d67cec666bfe
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-04 17:00:47 +0200

    child-parent

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  data: string = "test"
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @Output() titleEvent = new EventEmitter<string>()
+
+  setTitle() {
+    this.titleEvent.emit(this.data)
   }
 
 }
